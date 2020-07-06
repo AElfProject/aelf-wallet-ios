02df837b3b6208fdfd3a2e1aab1120042c7565d0
wudameinv <huihui.wu@aelf.io>
2020-07-06 18:10:34 +0800

    app-center-pack

--- Aelf/appcenter-post-clone.sh
+++ Aelf/appcenter-post-clone.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+git clone https://github.com/AElfProject/aelf-wallet-ios.git Pods
+cd Pods/Pods
+git checkout pods-only
+cd ../../
 brew install autoconf automake libtool
 echo "Uninstalling all CocoaPods versions"
 sudo gem uninstall cocoapods --all --executables
