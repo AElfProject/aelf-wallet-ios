aea1083b84e12ace5257f1118dc89de2db57586b
aelf-hz <mason.huang@aelf.io>
2020-11-12 19:25:35 +0800

    test

--- Aelf/appcenter-post-clone.sh
+++ Aelf/appcenter-post-clone.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+git clone https://github.com/AElfProject/aelf-wallet-ios.git Pods-only
+cd Pods-only
+git checkout pods-only
+mv Pods ../
+cd ../
 brew install autoconf automake libtool
-echo "Installing CocoaPods"
+echo "Uninstalling all CocoaPods versions"
+sudo gem uninstall cocoapods --all --executables
+COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
+echo "Installing CocoaPods version $COCOAPODS_VER"
 sudo gem install cocoapods
-pod repo update && pod install
+app_version=`cat AelfApp.xcodeproj/project.pbxproj | grep MARKETING_VERSION |  awk -F"=|;| "  '{ print $4 }' | head -1`
+echo $app_version
